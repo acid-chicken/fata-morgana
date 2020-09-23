e0e2b3953172613f7c8dc53f921f5f0e074dc6c2
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2020-09-24 04:56:29 +0900

    Create Program.Constant.cs

---
+++ src/FataMorgana/Program.Constant.cs
@@ -0,0 +1,15 @@
+#nullable enable
+
+using System;
+
+namespace AcidChicken.FataMorgana
+{
+    partial class Program
+    {
+        const string A = "a";
+        const string E = "e";
+        const string I = "i";
+        const string O = "o";
+        const string U = "u";
+    }
+}
